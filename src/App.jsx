8f6ac06b57b54680bd5da1235ff4143f3defbabc
roqueCoronel <rokecoronel748@gmail.com>
2022-03-23 10:15:36 -0300

    se dieron los uiltmos push al app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@
 import { useState,useEffect } from 'react'
 import styled  from '@emotion/styled'
 import Formluario from './components/Formulario'
+import Resultado from './components/Resultado'
+import Spinner from './components/Spinner'
+
 import ImagenCripto from './img/imagen-criptos.png'
  const Contenedor = styled.div`
       max-width:900px;
@@ -46,11 +49,15 @@ function App() {
 
   const [monedas,setMonedas] = useState({})
   const [resultado,setResultado] = useState({})
+  const [cargando,setCargando] = useState(false)
+
 
 
   useEffect(() => {
         if(Object.keys(monedas).length > 0){
          const cotizarCripto = async () => {
+           setCargando(true)
+           setResultado({});
            const {moneda,criptomoneda}  = monedas;
           const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
 
@@ -58,7 +65,8 @@ function App() {
           const resultado = await respuesta.json();
 
           setResultado(resultado.DISPLAY[criptomoneda][moneda]);
-
+            
+          setCargando(false)
          };
          cotizarCripto();
         }
@@ -76,6 +84,9 @@ function App() {
         <Formluario
            setMonedas={setMonedas}
         />   
+        {cargando && <Spinner/>}
+
+        {resultado.PRICE && <Resultado  resultado={resultado}/>}
       </div>
     </Contenedor>
     )
